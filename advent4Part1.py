951ee0ab52a180da7da296655df58295a3d88171
KierankThompson <thompson.kierank@gmail.com>
2023-12-02 20:30:04 -0500

    fixed advent4Part1

--- advent4Part1.py
+++ advent4Part1.py
@@ -2,11 +2,11 @@ s = open("advent4.txt","r")
 ans =  0
 for i in s:
     i3 = i.split(",")[0]
-    i4 = i.split(",")1]
+    i4 = i.split(",")[1]
     p1Min = i3.split("-")[0]
     p1Max = i3.split("-")[1]
     p2Min = i4.split("-")[0]
     p2Max = i4.split("-")[1]
-    if (p2Max >= p1Max and p2Min <= p1Min) or (p1Max >= p2Max and p1Min <= p2Min):
+    if (int(p2Max) >= int(p1Max) and int(p2Min) <= int(p1Min)) or (int(p1Max) >= int(p2Max) and int(p1Min) <= int(p2Min)):
         ans += 1
 print(ans)
