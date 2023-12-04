109522438493d2295d1a25929acbeda2e3418a2c
KierankThompson <thompson.kierank@gmail.com>
2023-12-04 07:47:12 -0500

    Finished day 5

---
+++ advent5Part1.py
@@ -0,0 +1,27 @@
+s = open("advent5.txt","r")
+s = s.readlines()
+arr = [[] for _ in range(10)]
+startAdding = False
+for i in s:
+    if i == "\n":
+        startAdding = True
+        continue
+    if startAdding == False:
+        for j in range(len(i)):
+            if startAdding == False and i[j].isalpha():
+                arr[int((j+3)/4)].append(i[j])
+    else:
+        nums = i.split()
+        amount = int(nums[1])
+        origin = int(nums[3])
+        destin = int(nums[5])
+        while amount > 0:
+            element = arr[origin].pop(0)
+            arr[destin].insert(0,element)
+            amount -= 1
+ans = ""
+for a in arr:
+    for a2 in a:
+        ans += a2
+        break
+print(ans)
